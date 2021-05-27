fb92439d6971d111f9d113a6c4aaf3816be3e501
David Nguyen-Huu <nguye@DESKTOP-6OB9G0V>
2021-05-27 17:06:13 -0400

    commit 1

--- day2.py
+++ day2.py
@@ -11,11 +11,3 @@ def solve(meal_cost, tip_percent, tax_percent):
     total_cost = meal_cost + tip + tax
     print(round(total_cost));
 
-if __name__ == '__main__':
-    meal_cost = float(input().strip())
-
-    tip_percent = int(input().strip())
-
-    tax_percent = int(input().strip())
-
-    solve(meal_cost, tip_percent, tax_percent)
